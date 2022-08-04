0f8a39c62762514738ba4569ef7d5a79586f2c90
juss-10 <100239793+juss-10@users.noreply.github.com>
2022-08-04 14:37:10 +0200

    Add game round input

---
+++ assets/scripts/index.js
@@ -0,0 +1,82 @@
+
+const handElements = [...document.querySelectorAll(".game-hand-container")];
+let playerPoints = 0;
+let computerPoints = 0;
+
+handElements.forEach(hand => hand.addEventListener("click", renderGame))
+
+function getComputerChoice() {
+    const gameChoices = ["rock", "paper", "scissors"];
+    const computerChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
+    return getPlayerObject(computerChoice);
+}
+
+function getPlayerChoice(e) {
+    const playerChoice = e.target.id;  
+    return getPlayerObject(playerChoice)
+}
+
+function getPlayerObject(playerChoice) {
+    const playerObject = {
+        hand: "",
+        emoji: ""
+    };
+
+    if (playerChoice === "rock") {
+        playerObject.hand = "rock";
+        playerObject.emoji = "🪨";
+    } else if (playerChoice === "paper") {
+        playerObject.hand = "paper";
+        playerObject.emoji = "📃";
+    } else {
+        playerObject.hand = "scissors";
+        playerObject.emoji = "✂️";
+    }
+
+    return playerObject;
+}
+
+function renderGame(e) {
+    const round = playRound(getPlayerChoice(e), getComputerChoice());
+    displayRound(round)
+}
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice.hand === computerChoice.hand) {
+        return {
+            winner: null,
+            tied: true,
+            playerChoice,
+            computerChoice
+        }
+    }
+
+    let winner;
+
+    switch (playerChoice.hand) {
+        case "rock":
+            winner = (computerChoice === "paper") ? "computer" : "player";
+            break;
+        case "paper":
+            winner = (computerChoice === "scissors") ? "computer" : "player";
+            break;
+        case "scissors":
+            winner = (computerChoice === "rock") ? "computer" : "player";
+            break;
+    }
+
+    return {
+        winner,
+        tied: false,
+        playerChoice,
+        computerChoice
+    }
+}
+
+function displayRound(round) {
+    const playerHand = document.querySelector("#player-hand");
+    const computerHand = document.querySelector("#computer-hand");
+
+    playerHand.textContent = round.playerChoice.emoji;
+    computerHand.textContent = round.computerChoice.emoji;
+}
